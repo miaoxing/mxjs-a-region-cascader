0e970b1b457dba3b0b22650670e190269109e6eb
twinh <twinhuang@qq.com>
2021-03-16 23:09:43 +0800

    feat: 支持传入 `value`，增加 `parentId`，`url` 和 `maxLevel` 选项

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import {Cascader} from 'antd';
 import PropTypes from 'prop-types';
 import {FormContext} from '@mxjs/a-form';
 import $ from 'miaoxing';
+import appendUrl from 'append-url';
 
 export default class RegionCascader extends React.Component {
   static contextType = FormContext;
 
   static propTypes = {
     fieldNames: PropTypes.object,
+    parentId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    url: PropTypes.string,
+    maxLevel: PropTypes.number,
   }
 
   static defaultProps = {
@@ -16,19 +20,69 @@ export default class RegionCascader extends React.Component {
       label: 'shortName',
       value: 'shortName',
       children: 'children'
-    }
+    },
+    parentId: 0,
+    url: $.url('api/regions', {virtual: 0}),
+    maxLevel: 3,
   }
 
   state = {
     options: [],
   };
 
+  caches = {};
+
   async componentDidMount() {
-    const ret = await this.getRegions();
-    ret.data.forEach(row => {
-      row.isLeaf = !row.hasChildren;
-    });
-    this.setState({options: ret.data});
+    await this.loadRegions();
+  }
+
+  async loadRegions(values = []) {
+    let options = [];
+    let parentOptions = [];
+    let level = 0;
+
+    loop:
+      for (const value of [this.props.parentId].concat(values)) {
+        level++;
+        let matchOption;
+        for (const option of parentOptions) {
+          if (option[this.props.fieldNames.value] === value) {
+            // 如果当前选项没有子节点，则跳出不再查询下属节点
+            if (option.isLeaf) {
+              break loop;
+            }
+
+            // 记录当前的选项
+            matchOption = option;
+          }
+        }
+
+        // 优先使用 id 去查询，没有 id 才使用 value
+        const data = await this.getRegionFromCache(matchOption ? matchOption.id : value);
+        if (level >= this.props.maxLevel) {
+          data.forEach(row => {
+            row.isLeaf = true;
+          });
+        }
+
+        if (value === this.props.parentId) {
+          // 第一级的数据直接记录起来
+          options = data;
+        } else {
+          matchOption.children = data;
+        }
+
+        // 记录当前层级的数据，供下级数据比较
+        parentOptions = data;
+      }
+
+    this.setState({options});
+  }
+
+  async componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.value !== this.props.value) {
+      await this.loadRegions(this.props.value);
+    }
   }
 
   onChange = (value) => {
@@ -38,34 +92,33 @@ export default class RegionCascader extends React.Component {
     this.props.onChange(value);
   }
 
-  loadData = async (selectedOptions) => {
-    const isLeaf = selectedOptions.length >= 2;
-    const targetOption = selectedOptions[selectedOptions.length - 1];
-
-    targetOption.loading = true;
-    const ret = await this.getRegions(targetOption.id);
-    targetOption.loading = false;
-
-    ret.data.forEach(row => {
-      row.isLeaf = !row.hasChildren || isLeaf;
-    });
-    targetOption[this.props.fieldNames.children] = ret.data;
-    this.setState({
-      options: [...this.state.options],
-    });
+  loadData = () => {
+    // 留空，以便菜单不会自动关闭
   };
 
-  getRegions = async (parentId = 0) => {
-    return $.get($.url('api/regions', {virtual: 0, parentId: parentId}));
+  getRegionFromCache = async (parentId) => {
+    if (!this.caches[parentId]) {
+      this.caches[parentId] = this.getRegions(parentId);
+    }
+    return this.caches[parentId];
+  }
+
+  getRegions = async (parentId) => {
+    const ret = await $.get(appendUrl(this.props.url, {parentId}));
+    ret.data.forEach(row => {
+      row.isLeaf = !row.hasChildren;
+    });
+    return ret.data;
   }
 
   render() {
+    const {parentId, url, maxLevel, ...props} = this.props;
     return (
       <Cascader
         options={this.state.options}
         loadData={this.loadData}
         changeOnSelect
-        {...this.props}
+        {...props}
         onChange={this.onChange}
       />
     );
